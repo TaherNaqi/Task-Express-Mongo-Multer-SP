9368a1300cfc42d890acb725162070697e685612
TaherNaqi <eng-naqi@hotmail.com>
2022-02-02 16:21:03 +0300

    finished task

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const connectDb = require('./database');
-const productsRoutes = require('./api/products/routes');
-
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./database");
+const productsRoutes = require("./api/products/routes");
+const path = require("path");
+const logger = require("./middleware/logger");
 const app = express();
 connectDb();
 
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(
-    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-  );
-  next();
-});
+app.use(logger);
 
+app.use("/media", express.static(path.join(__dirname, "media")));
 // Routes
-app.use('/products', productsRoutes);
-
+app.use("/products", productsRoutes);
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
-    message: err.message || 'Internal Server Error',
+    message: err.message || "Internal Server Error",
   });
 });
-app.listen(process.env.PORT || 5000);
+app.listen(8000);
